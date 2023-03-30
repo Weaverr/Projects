b57a5e358357ac1e8e763ae3f16834a2bb584ab4
Jack Weaver <jackdweaver@btinternet.com>
2023-03-30 19:33:16 +0100

    v1.3

--- sketch.js
+++ sketch.js
@@ -5,10 +5,16 @@ let img;
 
 let songOptions = [  "toke",  "molly",  "24 songs",  "pop out",  "cmon",  "iloveuihateu",  "watch this",  "stop breathing",  "rokstar"];
 
-let albumOptions = [  { name: "up 2 me", songs: ["cmon", "morning mudd", "Song 3"] },
-  { name: "2 alive", songs: ["Song 4", "Song 5", "Song 6"] },
-  { name: "lyfe", songs: ["Song 7", "Song 8", "Song 9"] },
-  { name: "afterlyfe", songs: ["Song 7", "Song 8", "Song 9"] },
+let albumOptions = [  { name: "up 2 me", artist: "yeat", songs: ["cmon", "morning mudd", "got rich", "let ya know", "stayed tha same", "get busy"] },
+  { name: "2 alive", artist: "yeat", songs: ["Song 4", "Song 5", "Song 6"] },
+  { name: "lyfe", artist: "yeat", songs: ["Song 7", "Song 8", "Song 9"] },
+  { name: "afterlyfe", artist: "yeat", songs: ["Song 7", "Song 8", "Song 9"] },
+];
+
+let artistOptions = [
+  { name: "yeat", albums: ["up 2 me", "2 alive", "lyfe", "afterlyfe"] },
+  { name: "Artist 2", albums: ["x"] },
+  { name: "Artist 3", albums: ["x", "x"] },
 ];
 
 let playlistOptions = [  { name: "Playlist 1", songs: ["Song 1", "Song 4", "Song 7"] },
@@ -36,43 +42,72 @@ function setup() {
 
   select.option("Select a Category:");
   select.option("Songs");
-  select.option("Albums");
+  select.option("Artists");
   select.option("Playlists");
   select.option("Go Back");
 
   // update options when category is selected
   select.changed(function() {
-    let category = select.value();
-    if (category === "Songs") {
-      currentOptions = songOptions;
-      currentCategory = "Songs";
-    } else if (category === "Albums") {
-      currentOptions = albumOptions.map((album) => album.name);
-      currentCategory = "Albums";
-    } else if (category === "Playlists") {
-      currentOptions = playlistOptions.map((playlist) => playlist.name);
-      currentCategory = "Playlists";
-    } else if (category === "Go Back") {
-      select.remove();
-      setup();
-    }
-
+        let category = select.value();
+        if (category === "Songs") {
+          currentOptions = songOptions;
+          currentCategory = "Songs";
+        } else if (category === "Artists") {
+          currentOptions = artistOptions.map((artist) => artist.name);
+          currentCategory = "Artists";
+        } else if (category === "Playlists") {
+          currentOptions = playlistOptions.map((playlist) => playlist.name);
+          currentCategory = "Playlists";
+        } else if (category === "Go Back") {
+          select.remove();
+          setup();
+        }
+        
     let songSelect = document.querySelector(".song-select");
     if (songSelect) {
       songSelect.remove();
     }
-
-    // create new select element for songs or albums
-    if (category === "Songs" || category === "Albums") {
+    
+    if (category === "Songs" || category === "Albums" || category === "Artists") {
       songSelect = createSelect();
       songSelect.class("song-select");
       songSelect.style('font-size', '22px');
       songSelect.style('font-family', 'Arial');
       songSelect.position(windowWidth / 2 - 90, 50);
-
       songSelect.option("Select an Option:");
+
       for (let i = 0; i < currentOptions.length; i++) {
         songSelect.option(currentOptions[i]);
+      }
+      
+      songSelect.changed(function() {
+        let songName = songSelect.value();
+        if (currentSong) {
+          currentSong.stop();
+        
+        }
+        
+    if (currentCategory === "Songs") {
+    currentSong = loadSound(`sounds/${songName}.mp3`, function() {
+      currentSong.play();
+    });
+
+  } else if (currentCategory === "Albums") {
+    let album = albumOptions.find((album) => album.name === songName);
+    if (album) {
+      let songSelect = document.querySelector(".song-select");
+      songSelect.remove();
+      
+
+      songSelect = createSelect();
+      songSelect.class("song-select");
+      songSelect.style('font-size', '22px');
+      songSelect.style('font-family', 'Arial');
+      songSelect.position(windowWidth / 2 - 90, 90);
+
+      songSelect.option("Select a Song:");
+      for (let i = 0; i < album.songs.length; i++) {
+        songSelect.option(album.songs[i]);
       }
 
       songSelect.changed(function() {
@@ -80,52 +115,76 @@ function setup() {
         if (currentSong) {
           currentSong.stop();
         }
-        if (currentCategory === "Songs") {
-          currentSong = loadSound(`sounds/${songName}.mp3`, function() {
-            currentSong.play();
-          });
-        } else if (currentCategory === "Albums") {
-          let album = albumOptions.find((album) => album.name === songName);
-          if (album) {
-            let songSelect = document.querySelector(".song-select");
-            songSelect.remove();
+        currentSong = loadSound(`sounds/${songName}.mp3`, function() {
+          currentSong.play();
+        });
+      });
+    }
+  } else if (currentCategory === "Artists") {
+    let artist = artistOptions.find((artist) => artist.name === songName);
+    if (artist) {
+      let songSelect = document.querySelector(".song-select");
+      songSelect.remove();
+      
+      songSelect = createSelect();
+      songSelect.class("song-select");
+      songSelect.style('font-size', '22px');
+      songSelect.style('font-family', 'Arial');
+      songSelect.position(windowWidth / 2 - 90, 90);
 
-            songSelect = createSelect();
-            songSelect.class("song-select");
-            songSelect.style('font-size', '22px');
-            songSelect.style('font-family', 'Arial');
-            songSelect.position(windowWidth / 2 - 90, 90);
+      songSelect.option("Select an Album:");
+      for (let i = 0; i < artist.albums.length; i++) {
+        songSelect.option(artist.albums[i]);
+      }
 
-            songSelect.option("Select a Song:");
-            for (let i = 0; i < album.songs.length; i++) {
-              songSelect.option(album.songs[i]);
-            }
+      songSelect.changed(function() {
+        let albumName = songSelect.value();
+        if (currentSong) {
+          currentSong.stop();
+        }
+        let album = albumOptions.find((album) => album.name === albumName && album.artist === songName);
+        if (album) {
+          let songSelect = document.querySelector(".song-select");
+          songSelect.remove();
 
-            songSelect.changed(function() {
-              let songName = songSelect.value();
-              if (currentSong) {
-                currentSong.stop();
-              }
-              currentSong = loadSound(`sounds/${songName}.mp3`, function() {
-                currentSong.play();
-              });
-            });
+          songSelect = createSelect();
+          songSelect.class("song-select");
+          songSelect.style('font-size', '22px');
+          songSelect.style('font-family', 'Arial');
+          songSelect.position(windowWidth / 2 - 90, 130);
+
+          songSelect.option("Select a Song:");
+          for (let i = 0; i < album.songs.length; i++) {
+            songSelect.option(album.songs[i]);
           }
+
+          songSelect.changed(function() {
+            let songName = songSelect.value();
+            if (currentSong) {
+              currentSong.stop();
+            }
+            currentSong = loadSound(`sounds/${songName}.mp3`, function() {
+              currentSong.play();
+            });
+          });
         }
       });
     }
-  });
+  }
+});
+}
+});
 
-  // slider
-  slider = createSlider(0, 100, 0);
-  slider.position(windowWidth / 2 - 150, height - 100);
-  slider.style('width', '300px');
+// slider
+slider = createSlider(0, 100, 0);
+slider.position(windowWidth / 2 - 150, height - 100);
+slider.style('width', '300px');
 
-  runtime = createDiv();
-  runtime.position(windowWidth / 2 - 50, height - 70);
-  runtime.style('font-size', '20px');
-  runtime.style('font-family', 'Arial');
-  runtime.style('color', 'white');
+runtime = createDiv();
+runtime.position(windowWidth / 2 - 50, height - 70);
+runtime.style('font-size', '20px');
+runtime.style('font-family', 'Arial');
+runtime.style('color', 'white');
 }
   
 
